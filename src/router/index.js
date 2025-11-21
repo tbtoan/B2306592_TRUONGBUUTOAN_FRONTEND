ca433be0572f13b46af664e161bf712282b0f157
toanb2306592-create <toanb2306592@student.ctu.edu.vn>
2025-11-21 18:35:25 +0700

    Them mot lien he moi

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,12 @@ const routes = [
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
-    props: true // Truyền các biến trong $route.params vào làm props
+    props: true 
+  },
+  {
+    path: "/contacts", 
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
   },
 ];
 
