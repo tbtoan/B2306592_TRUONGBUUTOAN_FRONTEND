3ef993350a93fe3e240313ba3f73af2543586ff8
toanb2306592-create <toanb2306592@student.ctu.edu.vn>
2025-11-21 18:27:15 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -33,7 +33,18 @@
           Chi tiết Liên hệ
           <i class="fas fa-address-card"></i>
         </h4>
+        
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh
+          </span>
+        </router-link>
       </div>
     </div>
   </div>
@@ -128,7 +139,7 @@ export default {
       this.$router.push({ name: "contact.add" });
     },
   },
-
+  
   mounted() {
     this.refreshList();
   },
